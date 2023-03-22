64494d53d319dd1ef5018d7fa4ac1e6cc1f73282
facundobailo <facundo.bailo@gmail.com>
2023-03-22 03:19:56 -0300

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+
+export const useFetch = ( url ) => {
+
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasErorr: null
+    });
+
+    const getFetch = async() => {
+
+        setState({
+            ...state,
+            isLoading: true,
+        })
+        
+        const resp = await fetch( url );
+        const data = await resp.json();
+
+        setState({
+            data,
+            isLoading: false,
+            hasErorr: null,
+
+        })
+
+    }
+
+    useEffect(() => {
+        
+        getFetch()
+        
+        return () => {
+            
+        };
+
+    }, [url]);
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasErorr: state.hasErorr,
+  }
+}
